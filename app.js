7178fd5e98d48d1b10305c3579bdc47c94c60ab5
Stian Veum Møllersen <stian.moellersen@gmail.com>
2013-03-18 14:18:47 +0100

    Adding basic configuration for heroku deployment

--- app.js
+++ app.js
@@ -30,8 +30,11 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
